9965a91f7e4d84bdacf17c2483e6ca27533ff004
Arghadeep Chakraborty <argho.chakr@gmail.com>
2024-03-29 04:56:31 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Arghadeep Chakraborty on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
